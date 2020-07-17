26dbb2f7c19e52e0baf9a119d8bcb5937b1c8357
Hiroki Kondo <kondo.matr02@gmail.com>
2020-07-17 20:13:21 +0900

    Ep1完了

---
+++ ChatAppWithFirebase/ChatlistViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ChatlistViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by 近藤宏輝 on 2020/07/17.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+    
+    private let cellId = "cellId"
+    
+    @IBOutlet var chatListTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chatListTableView.delegate = self
+        chatListTableView.dataSource = self
+    }
+}
+
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ChatListViewController:UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatListTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        return cell
+    }
+}
+
+
+class ChatListTableViewCell: UITableViewCell {
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var partnerLable: UILabel!
+    @IBOutlet var userImageView: UIImageView!
+    @IBOutlet var latestMessageLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userImageView.layer.cornerRadius = 35
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
