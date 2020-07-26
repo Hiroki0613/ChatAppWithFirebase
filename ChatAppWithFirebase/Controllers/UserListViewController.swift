5936ef45de0e35f2240598edf8c945805223da0c
Hiroki Kondo <kondo.matr02@gmail.com>
2020-07-26 23:40:40 +0900

    #9途中

---
+++ ChatAppWithFirebase/Controllers/UserListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UserListViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by 近藤宏輝 on 2020/07/26.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
